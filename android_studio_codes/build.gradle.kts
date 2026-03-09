ed373abfde77d3fec7c14a6d05333b7d48374dd9
Leila-dev <nexusovertex@gmail.com>
2026-03-09 16:32:27 +0100

    Add files via upload

---
+++ android_studio_codes/build.gradle.kts
@@ -0,0 +1,12 @@
+
+
+// ================================================
+// build.gradle.kts  —  PROJECT ROOT
+// ================================================
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
